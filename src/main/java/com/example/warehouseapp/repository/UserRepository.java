6fe965767adcf34a7e3994dcf4c9138b4cf93412
ZuhriddinShamsiddinov <sh.zuhriddin.2005@gmail.com>
2022-03-11 15:20:18 +0500

    added

---
+++ src/main/java/com/example/warehouseapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.warehouseapp.repository;
+
+import com.example.warehouseapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
---
+++ src/main/java/com/example/warehouseapp/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.warehouseapp.service;
+
+import com.example.warehouseapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+}
