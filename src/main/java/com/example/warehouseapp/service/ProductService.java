5c84984f53373244a6034d8e74114689f049ba4c
dlidobriy <dlidobriy1@gmail.com>
2022-03-12 22:57:51 +0500

    Product crudi tugatim

--- src/main/java/com/example/warehouseapp/service/ProductService.java
+++ src/main/java/com/example/warehouseapp/service/ProductService.java
@@ -1,16 +1,24 @@
 package com.example.warehouseapp.service;
 
 import com.example.warehouseapp.dto.ApiResponse;
+import com.example.warehouseapp.dto.ProductDTO;
+import com.example.warehouseapp.entity.Category;
 import com.example.warehouseapp.entity.Product;
+import com.example.warehouseapp.repository.CategoryRepository;
 import com.example.warehouseapp.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
     @Autowired
     ProductRepository productRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
+
 
 public ApiResponse add(Product product){
     Product save = productRepository.save(product);
@@ -18,4 +26,17 @@ public ApiResponse add(Product product){
   }
 
 
+    public ApiResponse edit(Integer id, ProductDTO productDTO) {
+        Optional<Product>optionalProduct=productRepository.findById(id);
+        Product product=optionalProduct.get();
+
+        Optional<Category>optionalCategory=categoryRepository.findById(productDTO.getCategoryId());
+        Category category=optionalCategory.get();
+
+        product.setName(productDTO.getName());
+        product.setCategory(category);
+
+     productRepository.save(product);
+     return new ApiResponse("Updated",true);
+    }
 }
